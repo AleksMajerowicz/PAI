a38380af1b6b577e31c1d7d96923b2c7ce18a6d0
Aleks Majerowicz <amajerowicz222@gmail.com>
2023-01-30 14:49:34 +0100

    Poprawka

--- PHP/tabliczkaMnożenia.php
+++ PHP/tabliczkaMnożenia.php
@@ -18,7 +18,14 @@
 echo "<table>";
 for($y = 1; $y <= 10; $y++)
 {
-    echo "<tr>";
+    if($y % 2 == 0)
+    {
+        echo "<tr style = 'background-color: red'>";
+    }
+    else
+    {
+        echo "<tr style = 'background-color: yellow'>";
+    }
     for($x = 1; $x < 10; $x++)
     {
         echo "<td>".$y * $x."</td>";
