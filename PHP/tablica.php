b2334f54aa48eb1fdfd1e8d6e66815d2c4bf5aab
Aleks Majerowicz <amajerowicz222@gmail.com>
2023-02-06 14:10:59 +0100

    Zadanie domowe NIEDOKOŃCZONE

---
+++ PHP/tablica.php
@@ -0,0 +1,17 @@
+<?php
+    $zwierzeta["Chomik"] = "Hamster";
+    $zwierzeta["Kot"] = "Cat";
+    $zwierzeta["Pies"] = "Dog";
+    $zwierzeta["Papuga"] = "Parrot";
+    $zwierzeta["Lew"] = "Lion";
+    $zwierzeta["Wilk"] = "Wolf";
+    $zwierzeta["Słoń"] = "Elephan";
+    $zwierzeta["Małpa"] = "Monkey";
+    $zwierzeta["Mrówka"] = "Ant";
+    $zwierzeta["Wąż"] = "Snake";
+
+    foreach($animal as $zwierzeta)
+    {
+        echo "$animal <br>";
+    }
+?>
